ce2f4f8391dc30c109198712871d36348eefd58d
Franco Rossi <FRossi@sofrecom.com.ar>
2020-08-22 17:14:23 -0300

    Home, search & user components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './routes/search/search.component';
+import { HomeComponent } from './routes/home/home.component';
+import { UserComponent } from './routes/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchComponent, HomeComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
