ecb47cb03748c7688beba07bba0b6211e5660fe6
Franco Rossi <FRossi@sofrecom.com.ar>
2020-08-22 19:13:40 -0300

    Bootstrap styles added

--- src/app/components/user-block/user-block.component.ts
+++ src/app/components/user-block/user-block.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-user-block',
   templateUrl: './user-block.component.html',
   styleUrls: ['./user-block.component.scss'],
+  host: { class: 'col-12 col-md-6 col-xl-3' },
 })
 export class UserBlockComponent implements OnInit {
   @Input() user;
